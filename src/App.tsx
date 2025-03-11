e42f11d4dbeb318e10f3cccec84936c993cdf80d
elloimbrandon <elloimbrandon@gmail.com>
2025-03-11 10:43:12 -0700

    added vite config for routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/views/Home';
 import NotFound from './components/views/NotFound';
 
-// TODO:
-// - add new bio picture
-
 export default function App() {
   return (
     <Router>
